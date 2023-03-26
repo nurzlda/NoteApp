7c0d6d1f981e5c512538cfb07d5394f80ccc3333
nurzida <nurzidaulanbekovna@gmail.com>
2023-03-26 09:17:19 +0300

    Mofified domain/build.gradle.kts

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
-    id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+    id(Plugins.Kotlin.jvm)
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_7
     targetCompatibility = JavaVersion.VERSION_1_7
+}
+
+dependencies{
+    implementation(Dependencies.javax)
+    implementation(Dependencies.Coroutine.coroutinesCore)
 }
